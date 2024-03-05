cf91a8e1fe0772bc0bdfca3807a024aa3aa597cb
Shobith S <shobithsugumar@gmail.com>
2024-03-05 23:39:22 +0530

    peer to peer con

--- public/script.js
+++ public/script.js
@@ -1,9 +1,18 @@
 const socket = io("/");
-const vidoeGrid = document.getElementById("video-grid");
+const videoGrid = document.getElementById("video-grid");
+
+//peer connection where we recieve and create connection
+const peer = new Peer(undefined, {
+  //this is the path that we did in server.js
+  //path: "/peerjs",
+  host: "/", //what ever like localhost or any hosting page
+  port: "3040",
+});
+
+let myVideoStream;
 const myvideo = document.createElement("video");
 myvideo.muted = true;
 
-let myVideoStream;
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -11,24 +20,49 @@ navigator.mediaDevices
   })
   .then((stream) => {
     myVideoStream = stream;
-    addVideoStream(myvideo, myVideoStream);
-  });
-//we are req the room id to the server
-socket.emit("join-room", ROOMID);
+    addVideoStream(myvideo, stream);
 
-// we are respoding to the request
-socket.on("user-connected", () => {
-  connectToNewUser();
+    //answer the incoming call
+    peer.on("call", (call) => {
+      call.answer(stream); // Answer the call with an A/V stream.
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    // we are respoding to the request which came from the server
+    socket.on("user-connected", (userid) => {
+      console.log("new user connected", userid);
+      connectToNewUser(userid, stream);
+    });
+  });
+
+//we are listening to the peer connection
+peer.on("open", (id) => {
+  //we are req the room id to the server
+  socket.emit("join-room", ROOMID, id);
 });
 
-const connectToNewUser = () => {
-  console.log("new user");
-};
+function connectToNewUser(userid, stream) {
+  //Call another peer(ie another user) by calling peer.call with the peer ID of the destination peer.
+  //When a peer calls you, the call event is emitted.
+  //the Stream is my stream not the other user stream
+  const call = peer.call(userid, stream);
+  const video = document.createElement("video");
+  //this stream is users stream
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+}
 
-const addVideoStream = (video, stream) => {
+function addVideoStream(video, stream) {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
     video.play();
   });
-  vidoeGrid.append(video);
-};
+  videoGrid.append(video);
+}
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const { Server } = require("socket.io");
-const http = require("http");
 const app = express();
-const server = http.createServer(app);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
-const io = new Server(server);
+/*
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});*/
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
+
+//peerjs route
+//this path we will mention in script.js as path
+//app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -22,17 +29,18 @@ app.get("/:room", (req, res) => {
 
 io.on("connection", (socket) => {
   //the req that came from the client we are respoding
-  socket.on("join-room", (roomid) => {
+  socket.on("join-room", (roomid, userid) => {
     //the user joins the room
 
     socket.join(roomid);
-    console.log("the room id is", roomid);
+    console.log("the room id and userid", roomid, userid);
     //in this line we are requesting to the client.using roomid we select the specific room and with to we create a namesapce(so the user within the roomid can interact)
     //.the brodcast is used to tell all the user in the room that a new user has been connected.
-    socket.broadcast.emit("user-connected");
+    socket.broadcast.emit("user-connected", userid);
   });
 });
 
-server.listen(3000, () => {
-  console.log("app is runnig on port 3000");
+const port = 3000;
+server.listen(port, () => {
+  console.log(`app is runnig on port ${port}`);
 });
